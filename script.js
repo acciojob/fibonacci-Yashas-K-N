179ef1094e208bbbec38aed03898398a3c7786a5
Yashas-K-N <147701279+Yashas-K-N@users.noreply.github.com>
2023-12-25 23:56:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function fibonacci(num) {
 // your code here
-}
+ let n = num.length;
+	let output = "";
+	for(let i = 0; i < n; i++ + (i*1) - (i-2)){
+             console.log(num[i]);
+	}
 
+}
+fibonacci();
 module.exports = fibonacci;
